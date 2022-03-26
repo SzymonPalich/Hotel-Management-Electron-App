f1ed5668398ab143f663644cc7500960af7d2e8a
ppaluch099 <56819476+ppaluch099@users.noreply.github.com>
2022-03-26 22:51:07 +0100

    Maidenance done

--- Client/src/views/MinibarView.vue
+++ Client/src/views/MinibarView.vue
@@ -13,261 +13,184 @@ export default class MinibarView extends Vue {
 }
 </script>
 <template>
-  <form class="w-full max-w-sm">
-    <div
-      class="
-        bg-gray-800
-        border-4 border-solid border-gray-800
-        rounded-xl
-        text-white
-      "
-    >
-      <div class="px-4 py-5 sm:px-6 mt-2">
-        <h1 class="text-2xl leading-6 font-medium text-white text-center">
-          Podgląd
-        </h1>
-      </div>
-      <div class="md:flex md:items-center mb-4">
-        <div class="md:w-1/3">
-          <label
-            class="
-              block
-              text-gray-500
-              font-bold
-              md:text-right
-              mb-1
-              md:mb-0
-              pr-4
-            "
-            for="rum"
-          >
-            Rum
-          </label>
+  <div class="relative w-full flex flex-col h-screen overflow-hidden">
+    <div class="rounded-xl mx-80 mt-6">
+      <div
+        class="
+          bg-gray-800
+          border-4 border-solid border-gray-800
+          rounded-xl
+          text-white
+        "
+      >
+        <div class="px-4 py-5 sm:px-6 mt-2">
+          <h1 class="text-2xl leading-6 font-medium text-white text-center">
+            Pokój: {{ nr }}
+          </h1>
         </div>
-        <div class="md:w-2/3">
-          <input
-            class="
-              bg-gray-200
-              appearance-none
-              border-2 border-gray-200
-              rounded
-              w-full
-              py-2
-              px-4
-              text-gray-700
-              leading-tight
-              focus:outline-none focus:bg-white focus:border-purple-500
-            "
-            id="rum"
-            type="number"
-            min="0"
-            max="10"
-          />
-        </div>
-      </div>
-      <div class="md:flex md:items-center mb-6">
-        <div class="md:w-1/3">
-          <label
-            class="
-              block
-              text-gray-500
-              font-bold
-              md:text-right
-              mb-1
-              md:mb-0
-              pr-4
-            "
-            for="whisky"
-          >
-            Whisky
-          </label>
-        </div>
-        <div class="md:w-2/3">
-          <input
-            class="
-              bg-gray-200
-              appearance-none
-              border-2 border-gray-200
-              rounded
-              w-full
-              py-2
-              px-4
-              text-gray-700
-              leading-tight
-              focus:outline-none focus:bg-white focus:border-purple-500
-            "
-            id="whisky"
-            type="number"
-            min="0"
-            max="10"
-          />
-        </div>
-      </div>
-      <div class="md:flex md:items-center mb-6">
-        <div class="md:w-1/3">
-          <label
-            class="
-              block
-              text-gray-500
-              font-bold
-              md:text-right
-              mb-1
-              md:mb-0
-              pr-4
-            "
-            for="wodka"
-          >
-            Wódka
-          </label>
-        </div>
-        <div class="md:w-2/3">
-          <input
-            class="
-              bg-gray-200
-              appearance-none
-              border-2 border-gray-200
-              rounded
-              w-full
-              py-2
-              px-4
-              text-gray-700
-              leading-tight
-              focus:outline-none focus:bg-white focus:border-purple-500
-            "
-            id="wodka"
-            type="number"
-            min="0"
-            max="10"
-          />
-        </div>
-      </div>
-      <div class="md:flex md:items-center mb-6">
-        <div class="md:w-1/3">
-          <label
-            class="
-              block
-              text-gray-500
-              font-bold
-              md:text-right
-              mb-1
-              md:mb-0
-              pr-4
-            "
-            for="tequila"
-          >
-            Tequila
-          </label>
-        </div>
-        <div class="md:w-2/3">
-          <input
-            class="
-              bg-gray-200
-              appearance-none
-              border-2 border-gray-200
-              rounded
-              w-full
-              py-2
-              px-4
-              text-gray-700
-              leading-tight
-              focus:outline-none focus:bg-white focus:border-purple-500
-            "
-            id="tequila"
-            type="number"
-            min="0"
-            max="10"
-          />
-        </div>
-      </div>
-      <div class="md:flex md:items-center mb-6">
-        <div class="md:w-1/3">
-          <label
-            class="
-              block
-              text-gray-500
-              font-bold
-              md:text-right
-              mb-1
-              md:mb-0
-              pr-4
-            "
-            for="sake"
-          >
-            Sake
-          </label>
-        </div>
-        <div class="md:w-2/3">
-          <input
-            class="
-              bg-gray-200
-              appearance-none
-              border-2 border-gray-200
-              rounded
-              w-full
-              py-2
-              px-4
-              text-gray-700
-              leading-tight
-              focus:outline-none focus:bg-white focus:border-purple-500
-            "
-            id="sake"
-            type="number"
-            min="0"
-            max="10"
-          />
-        </div>
-      </div>
-      <div class="md:flex md:items-center">
-        <div class="md:w-1/3"></div>
-        <div class="md:w-3/3 mx-6">
-          <button
-            @click="swap()"
-            class="
-              shadow
-              bg-purple-500
-              hover:bg-purple-400
-              focus:shadow-outline focus:outline-none
-              text-white
-              font-bold
-              py-2
-              px-4
-              rounded
-            "
-            type="button"
-          >
-            Anuluj
-          </button>
-        </div>
-        <div class="md:w-3/3">
-          <button
-            @click="swap()"
-            class="
-              shadow
-              bg-purple-500
-              hover:bg-purple-400
-              focus:shadow-outline focus:outline-none
-              text-white
-              font-bold
-              py-2
-              px-4
-              rounded
-            "
-            type="button"
-          >
-            Akceptuj
-          </button>
+        <div class="bg-white h-full rounded-b-xl text-black">
+          <dl>
+            <div
+              class="
+                bg-gray-50
+                px-2
+                py-5
+                sm:grid sm:grid-cols-3 sm:gap-5 sm:px-6
+              "
+            >
+              <dt class="text-lg font-medium text-gray-500 text-right">Rum</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <input
+                  class="
+                    bg-gray-200
+                    appearance-none
+                    border-2 border-gray-200
+                    rounded
+                    w-full
+                    py-2
+                    px-4
+                    text-gray-700
+                    leading-tight
+                    focus:outline-none focus:bg-white focus:border-purple-500
+                  "
+                  id="rum"
+                  type="number"
+                  min="0"
+                  max="10"
+                />
+              </dd>
+            </div>
+            <div
+              class="
+                bg-gray-50
+                px-4
+                py-5
+                sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6
+              "
+            >
+              <dt class="text-lg font-medium text-gray-500 text-right">Whisky</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <input
+                  class="
+                    bg-gray-200
+                    appearance-none
+                    border-2 border-gray-200
+                    rounded
+                    w-full
+                    py-2
+                    px-4
+                    text-gray-700
+                    leading-tight
+                    focus:outline-none focus:bg-white focus:border-purple-500
+                  "
+                  id="whisky"
+                  type="number"
+                  min="0"
+                  max="10"
+                />
+              </dd>
+            </div>
+            <div
+              class="
+                bg-gray-50
+                px-4
+                py-5
+                sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6
+              "
+            >
+              <dt class="text-lg font-medium text-gray-500 text-right">Wódka</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <input
+                  class="
+                    bg-gray-200
+                    appearance-none
+                    border-2 border-gray-200
+                    rounded
+                    w-full
+                    py-2
+                    px-4
+                    text-gray-700
+                    leading-tight
+                    focus:outline-none focus:bg-white focus:border-purple-500
+                  "
+                  id="wodka"
+                  type="number"
+                  min="0"
+                  max="10"
+                />
+              </dd>
+            </div>
+            <div
+              class="
+                bg-gray-50
+                px-4
+                py-5
+                sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6
+              "
+            >
+              <dt class="text-lg font-medium text-gray-500 text-right">Tequila</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <input
+                  class="
+                    bg-gray-200
+                    appearance-none
+                    border-2 border-gray-200
+                    rounded
+                    w-full
+                    py-2
+                    px-4
+                    text-gray-700
+                    leading-tight
+                    focus:outline-none focus:bg-white focus:border-purple-500
+                  "
+                  id="tequila"
+                  type="number"
+                  min="0"
+                  max="10"
+                />
+              </dd>
+            </div>
+            <div
+              class="
+                bg-gray-50
+                px-4
+                py-5
+                sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6
+              "
+            >
+              <dt class="text-lg font-medium text-gray-500 text-right">Sake</dt>
+              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                <input
+                  class="
+                    bg-gray-200
+                    appearance-none
+                    border-2 border-gray-200
+                    rounded
+                    w-full
+                    py-2
+                    px-4
+                    text-gray-700
+                    leading-tight
+                    focus:outline-none focus:bg-white focus:border-purple-500
+                  "
+                  id="sake"
+                  type="number"
+                  min="0"
+                  max="10"
+                />
+              </dd>
+            </div>
+          </dl>
+          <div class="text-center text-white rounded-xl pb-4">
+              <button @click="swap()" class="border-2 px-2 py-2 rounded-xl bg-red-800 mr-3">
+                Anuluj
+              </button>
+              <button @click="swap()" class="border-2 px-2 py-2 rounded-xl bg-purple-800">
+                Akceptuj
+              </button>
+            </div>
         </div>
       </div>
     </div>
-  </form>
+  </div>
 </template>
-
-<style scoped>
-template {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-</style>
