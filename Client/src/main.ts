f502fb34a5abbcd502f25031ec46161d7b8a71b1
Przemysław Paluch <pp113754@stud.ur.edu.pl>
2022-03-30 19:42:21 +0200

    I-Conky fix

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import 'material-design-icons/iconfont/material-icons.css'
 
 const app = createApp(App)
 
 app.use(router)
-
-// createApp(App).use(router).mount('#app')
 
 app.mount('#app')
