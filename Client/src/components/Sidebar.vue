4b2689de48139bf24851c674d48351a7f82a4f83
Krzysztof Przewoźnik <kp113762@stud.ur.edu.pl>
2022-04-03 17:53:17 +0200

    naprawa ikony wylogowywania

--- Client/src/components/Sidebar.vue
+++ Client/src/components/Sidebar.vue
@@ -8,8 +8,8 @@
         </div>
       </div>
       <ul class="nav_list">
-        <li v-if="this.isActive('clients') " class="router-link-active">
-          <router-link to="/clients" >
+        <li v-if="this.isActive('clients')" class="router-link-active">
+          <router-link to="/clients">
             <i class="material-icons">people</i>
             <span class="links_name">Klienci</span>
           </router-link>
@@ -20,7 +20,7 @@
             <span class="links_name">Klienci</span>
           </router-link>
         </li>
-        <li v-if="this.isActive('employees') " class="router-link-active">
+        <li v-if="this.isActive('employees')" class="router-link-active">
           <router-link to="/employees">
             <i class="material-icons">contact_mail</i>
             <span class="links_name">Pracownicy</span>
@@ -32,49 +32,49 @@
             <span class="links_name">Pracownicy</span>
           </router-link>
         </li>
-        <li v-if="this.isActive('rooms') " class="router-link-active">
+        <li v-if="this.isActive('rooms')" class="router-link-active">
           <router-link to="/rooms">
             <i class="material-icons">home</i>
             <span class="links_name">Pokoje</span>
           </router-link>
         </li>
-            <li v-else>
+        <li v-else>
           <router-link to="/rooms">
             <i class="material-icons">home</i>
             <span class="links_name">Pokoje</span>
           </router-link>
         </li>
-         <li v-if="this.isActive('cleaning') " class="router-link-active">
+        <li v-if="this.isActive('cleaning')" class="router-link-active">
           <router-link to="/cleaning">
             <i class="material-icons">rowing</i>
             <span class="links_name">Pokojówka</span>
           </router-link>
         </li>
-            <li v-else>
+        <li v-else>
           <router-link to="/cleaning">
             <i class="material-icons">rowing</i>
             <span class="links_name">Pokojówka</span>
           </router-link>
         </li>
-        <li v-if="this.isActive('repairs') " class="router-link-active">
+        <li v-if="this.isActive('repairs')" class="router-link-active">
           <router-link to="/repairs">
             <i class="material-icons">build</i>
             <span class="links_name">Naprawy</span>
           </router-link>
         </li>
-           <li v-else>
+        <li v-else>
           <router-link to="/repairs">
             <i class="material-icons">build</i>
             <span class="links_name">Naprawy</span>
           </router-link>
         </li>
-        <li v-if="this.isActive('storage') " class="router-link-active">
+        <li v-if="this.isActive('storage')" class="router-link-active">
           <a href="#">
             <i class="material-icons">storage</i>
             <span class="links_name">Magazyn</span>
           </a>
         </li>
-           <li v-else>
+        <li v-else>
           <a href="#">
             <i class="material-icons">storage</i>
             <span class="links_name">Magazyn</span>
@@ -86,19 +86,19 @@
             <span class="links_name">Przychody</span>
           </a>
         </li>
-          <li v-else>
-        <a href="#">
-          <i class="material-icons">attach_money</i>
-          <span class="links_name">Przychody</span>
-        </a>
-      </li>
+        <li v-else>
+          <a href="#">
+            <i class="material-icons">attach_money</i>
+            <span class="links_name">Przychody</span>
+          </a>
+        </li>
         <li v-if="this.isActive('reports')" class="router-link-active">
           <a href="#">
             <i class="material-icons">receipt</i>
             <span class="links_name">Raporty</span>
           </a>
         </li>
-            <li v-else>
+        <li v-else>
           <a href="#">
             <i class="material-icons">receipt</i>
             <span class="links_name">Raporty</span>
@@ -112,10 +112,12 @@
               <div class="name">Jan Spurdziński</div>
               <div class="job">Menadżer</div>
             </div>
+            <div class="logout_icon">
+            <router-link to="/">
+              <img src="../../public/css/fonts/outline_logout_white_18dp.png" />
+            </router-link>
+            </div>
           </div>
-          <router-link to="/">
-            <i class="bx bx-log-out" id="log_out"></i>
-          </router-link>
         </div>
       </div>
     </div>
