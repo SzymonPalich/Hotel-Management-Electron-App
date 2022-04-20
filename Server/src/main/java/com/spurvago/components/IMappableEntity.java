3c3f96dc40ad4a3503b0784c7f6df3d8d1c78a78
Szymon Palich <theszymon450@gmail.com>
2022-04-20 14:50:25 +0200

    [SPR-63] Migracja, klasa klient

---
+++ Server/src/main/java/com/spurvago/components/IMappableEntity.java
@@ -0,0 +1,6 @@
+package com.spurvago.components;
+
+public interface IMappableEntity<T> {
+
+    void map(T source);
+}
---
+++ Server/src/main/java/com/spurvago/server/client/Client.java
@@ -0,0 +1,42 @@
+package com.spurvago.server.client;
+
+import com.spurvago.components.IMappableEntity;
+import lombok.Getter;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Entity
+@Table
+public class Client implements IMappableEntity<Client> {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Getter
+    private Long id;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private String surname;
+
+    @Getter
+    @Setter
+    private String email;
+
+    @Getter
+    @Setter
+    private String phoneNumber;
+
+    public Client() {
+    }
+
+    @Override
+    public void map(Client source) {
+        this.setName(source.getName());
+        this.setSurname(source.getSurname());
+        this.setEmail(source.getEmail());
+        this.setPhoneNumber(source.getPhoneNumber());
+    }
+}
