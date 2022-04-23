3eed36ca7298dd5187e7da0e9b4b0c6e267c1f51
Krzysztof Przewoźnik <kp113762@stud.ur.edu.pl>
2022-04-23 17:55:23 +0200

    autoryzacja, nie dzialaja jeszcze role

--- Server/src/main/java/com/spurvago/server/security/ApiController.java
+++ Server/src/main/java/com/spurvago/server/security/ApiController.java
@@ -1,4 +1,4 @@
-package com.spurvago.server;
+package com.spurvago.server.security;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
