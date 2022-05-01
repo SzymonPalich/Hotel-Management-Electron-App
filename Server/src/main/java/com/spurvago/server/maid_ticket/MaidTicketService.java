ee866ed23420bebd8795a6e5e8c2fff07b4c3a8a
Szymon Palich <sp113753@stud.ur.edu.pl>
2022-05-01 15:34:26 +0200

    fix fix

--- Server/src/main/java/com/spurvago/server/maid_ticket/MaidTicketService.java
+++ Server/src/main/java/com/spurvago/server/maid_ticket/MaidTicketService.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
