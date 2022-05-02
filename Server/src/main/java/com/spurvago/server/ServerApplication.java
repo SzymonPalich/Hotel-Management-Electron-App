0f7c15d0b9bcd158fcdb10472e4eef8351bef71c
Szymon Palich <sp113753@stud.ur.edu.pl>
2022-05-02 21:52:38 +0200

    dodano pliki do uruchamiania zeby nie zgubic, ostateczna wersja deploymentu na tomcata

--- Server/src/main/java/com/spurvago/server/ServerApplication.java
+++ Server/src/main/java/com/spurvago/server/ServerApplication.java
@@ -2,10 +2,11 @@ package com.spurvago.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
-public class ServerApplication {
+public class ServerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
