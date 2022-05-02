d32111f43e65bb71cd2a9cad40258cca170ef5d3
Szymon Palich <sp113753@stud.ur.edu.pl>
2022-05-02 21:55:46 +0200

    Revert "[SPR-72] Deployment na lokalny serwer Tomcat"
    This reverts commit 057fd4575cec1510d9cda0fe36230526728a46ce.

--- Server/src/main/java/com/spurvago/server/ApiController.java
+++ Server/src/main/java/com/spurvago/server/ApiController.java
@@ -1,11 +1,9 @@
 package com.spurvago.server;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 @RequestMapping(path = "api")
 public class ApiController {
--- Server/src/main/java/com/spurvago/server/ServerApplication.java
+++ Server/src/main/java/com/spurvago/server/ServerApplication.java
@@ -2,11 +2,10 @@ package com.spurvago.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
-public class ServerApplication extends SpringBootServletInitializer {
+public class ServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
