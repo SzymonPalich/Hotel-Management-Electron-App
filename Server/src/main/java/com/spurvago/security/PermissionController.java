dc9d57bef0e61930457c02d4f38eece552e9a2cd
Krzysztof Przewoźnik <kp113762@stud.ur.edu.pl>
2022-05-28 14:06:30 +0200

    poprawka

--- Server/src/main/java/com/spurvago/security/PermissionController.java
+++ Server/src/main/java/com/spurvago/security/PermissionController.java
@@ -1,25 +1,20 @@
-package com.spurvago.server.security;
+package com.spurvago.security;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import java.security.Principal;
 import java.util.Collection;
 
 @Controller
-public class SecurityController {
+public class PermissionController {
 
-    @RequestMapping(value = "/username", method = RequestMethod.GET)
+    @RequestMapping(value = "/roles", method = RequestMethod.GET)
     @ResponseBody
-    public Collection<? extends GrantedAuthority> currentUserName(Authentication authentication) {
-
+    public Collection<? extends GrantedAuthority> getUserAuthorities(Authentication authentication) {
         return authentication.getAuthorities();
     }
 }
