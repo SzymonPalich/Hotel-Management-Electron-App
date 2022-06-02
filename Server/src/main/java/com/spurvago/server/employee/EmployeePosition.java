6234b91fc1cfadeb29f3e2994187fc46ffbc37b3
Szymon Palich <sp113753@stud.ur.edu.pl>
2022-06-02 18:11:58 +0200

    TODO

--- Server/src/main/java/com/spurvago/server/employee/EmployeePosition.java
+++ Server/src/main/java/com/spurvago/server/employee/EmployeePosition.java
@@ -1,6 +1,7 @@
 package com.spurvago.server.employee;
 
 public enum EmployeePosition {
+    // TODO: Uzupełnić o pozostałe role
     MANAGER("ROLE_MANAGER");
 
     public final String stringValue;
