df2773e814c58c283b696066f91c6de61cdc7f16
Szymon Palich <sp113753@stud.ur.edu.pl>
2022-06-14 17:29:42 +0200

    Instalartor front

--- Client/src/background.ts
+++ Client/src/background.ts
@@ -64,34 +64,6 @@ app.on('activate', () => {
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.on('ready', async () => {
-  if (isDevelopment && !process.env.IS_TEST) {
-    // Install Vue Devtools
-    try {
-      await installExtension(VUEJS3_DEVTOOLS)
-    }
-    catch (e: unknown) {
-      if (typeof e === "string") {
-        console.error('Vue Devtools failed to install:', e.toString())
-      }
-      else {
-        console.error("Unknown error")
-      }
-    }
     createWindow()
-  }
 })
 
-// Exit cleanly on request from parent process in development mode.
-if (isDevelopment) {
-  if (process.platform === 'win32') {
-    process.on('message', (data) => {
-      if (data === 'graceful-exit') {
-        app.quit()
-      }
-    })
-  } else {
-    process.on('SIGTERM', () => {
-      app.quit()
-    })
-  }
-}
