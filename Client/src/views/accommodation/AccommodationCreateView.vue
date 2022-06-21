c1a2419957f3641ed1330eda522be4a321c7d717
Szymon Palich <sp113753@stud.ur.edu.pl>
2022-06-21 19:41:02 +0200

    szukajka lepsiejsza

--- Client/src/views/accommodation/AccommodationCreateView.vue
+++ Client/src/views/accommodation/AccommodationCreateView.vue
@@ -18,23 +18,31 @@
         </div>
         <div class="bg-white h-full rounded-b-xl text-black">
           <dl class="p-2">
-             <div
-              class="bg-gray-50 px-2 py-5 sm:grid sm:grid-cols-3 sm:gap-3 sm:px-6 relative"
-            > 
-              <div class="float-left" style='margin-right:2rem;'>
-                <dt class="text-sm font-medium text-gray-500">Początek rezerwacji</dt>
+            <div
+              class="
+                bg-gray-50
+                px-2
+                py-5
+                sm:grid sm:grid-cols-3 sm:gap-3 sm:px-6
+                relative
+              "
+            >
+              <div class="float-left" style="margin-right: 2rem">
+                <dt class="text-sm font-medium text-gray-500">
+                  Początek rezerwacji
+                </dt>
                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                   <input
                     class="
-                    border border-gray-300
-                    w-full
-                    h-full
-                    rounded-md
-                    px-2
-                    py-1_5
-                    outline-none
-                    focus:border focus:border-cyan-400 focus:rounded-md
-                  "
+                      border border-gray-300
+                      w-full
+                      h-full
+                      rounded-md
+                      px-2
+                      py-1_5
+                      outline-none
+                      focus:border focus:border-cyan-400 focus:rounded-md
+                    "
                     type="date"
                     min="new Date()"
                     required
@@ -43,8 +51,10 @@
                   />
                 </dd>
               </div>
-              <div class="float-left" style='margin-right:2rem;'>
-                <dt class="text-sm font-medium text-gray-500">Koniec rezerwacji</dt>
+              <div class="float-left" style="margin-right: 2rem">
+                <dt class="text-sm font-medium text-gray-500">
+                  Koniec rezerwacji
+                </dt>
                 <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
                   <input
                     class="
@@ -66,55 +76,86 @@
               </div>
               <div class="float-left sm:grid sm:grid-cols-3">
                 <div class="float-left sm:col-span-2">
-                <dt class="text-sm font-medium text-gray-500">Typ pokoju</dt>
-                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-                <v-select
-                  class="w-full"
-                  label="type"
-                  v-model="this.roomValue"
-                  :options="this.resultRoomTypes.content"
-                  :reduce="(option) => option.id"
-                  :clearable="false"
-                  @option:selecting="selectRoomTypeId"
-                  placeholder="Wybierz typ"
-                >
-                  <template v-slot:option="option">
-                    <span :class="option.icon"></span>
-                    {{ option.type }}
-                  </template>
-                  <template v-slot:no-options="{ search, searching }">
-                    <template v-if="searching">
-                      Brak wyników dla <em>{{ search }}</em
-                      >.
-                    </template>
-                  </template>
-                </v-select>
-              
-                </dd>
+                  <dt class="text-sm font-medium text-gray-500">Typ pokoju</dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    <v-select
+                      class="w-full"
+                      label="type"
+                      v-model="this.roomValue"
+                      :options="this.resultRoomTypes.content"
+                      :reduce="(option) => option.id"
+                      :clearable="false"
+                      @option:selecting="selectRoomTypeId"
+                      placeholder="Wybierz typ"
+                    >
+                      <template v-slot:option="option">
+                        <span :class="option.icon"></span>
+                        {{ option.type }}
+                      </template>
+                      <template v-slot:no-options="{ search, searching }">
+                        <template v-if="searching">
+                          Brak wyników dla <em>{{ search }}</em
+                          >.
+                        </template>
+                      </template>
+                    </v-select>
+                  </dd>
                 </div>
-                  <div class="float-right border-gray-400 rounded-2xl mt-6 ml-3">
-                  <img src="../../../public/css/fonts/icons8-search-25.png" class="align-middle material-icons" @click="filterRooms(this.result.startDate, this.result.endDate, this.roomValue)"/>
+                <div class="float-right border-gray-400 rounded-2xl mt-6 ml-3 content-between">
+                  <img
+                    src="../../../public/css/fonts/icons8-search-25.png"
+                    class="align-middle material-icons"
+                    @click="
+                      filterRooms(
+                        this.result.startDate,
+                        this.result.endDate,
+                        this.roomValue
+                      )
+                    "
+                  />
                 </div>
               </div>
             </div>
             <div class="border border-gray-300 px-2 py-0_1 outline-none mx-6">
               <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 p-2">
-                <select size="5" class="overflow-hidden w-full h-full border-0 outline-none focus:outline-none px-4 py-1" @change="selectRoom($event.target.value)" >
-                  <option class="hover:bg-gray-200 hover:rounded-xl focus:visible focus:bg-slate-500 focus:ring" v-for="room in resultRooms.content" :key="room" v-bind:value="room.id">
+                <select
+                  size="5"
+                  class="
+                    overflow-hidden
+                    w-full
+                    h-full
+                    border-0
+                    outline-none
+                    focus:outline-none
+                    px-4
+                    py-1
+                  "
+                  @change="selectRoom($event.target.value)"
+                >
+                  <option
+                    class="
+                      hover:bg-gray-200 hover:rounded-xl
+                      focus:visible focus:bg-slate-500 focus:ring
+                    "
+                    v-for="room in resultRooms.content"
+                    :key="room"
+                    v-bind:value="room.id"
+                  >
                     {{ room.roomLabel }}
                   </option>
                 </select>
               </dd>
             </div>
-            <div
-              class="
-                bg-gray-50
-                w-full
-                px-6
-                py-5
-              " style='margin-right:3rem;'
-            >
-              <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 grid-col" style="margin-right: 6.5rem;">
+            <div class="bg-gray-50 w-full px-6 py-5" style="margin-right: 3rem">
+              <dd
+                class="
+                  mt-1
+                  text-sm text-gray-900
+                  sm:mt-0 sm:col-span-2
+                  grid-col
+                "
+                style="margin-right: 6.5rem"
+              >
                 <v-select
                   label="clientLabel"
                   :options="this.resultClients"
@@ -252,7 +293,7 @@ export default defineComponent({
     selectRoomTypeId: function (value: number) {
       this.resultRooms.content = [];
       this.result.roomId = 0;
-      if(this.roomValue){
+      if (this.roomValue) {
         this.roomTypeId = this.roomValue;
       }
       this.$forceUpdate;
